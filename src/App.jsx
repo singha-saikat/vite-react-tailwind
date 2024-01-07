0a60ff3acd82da57c930fe5b85d1bcb2b5ebfe50
Saikat <sachinsingha7899@gmail.com>
2024-01-07 23:14:53 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className='text-center text-3xl text-red-500'>Welcome here react with TailwindCss </h1>
+    </>
+  )
+}
+
+export default App
