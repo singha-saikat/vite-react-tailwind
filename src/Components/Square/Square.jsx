57cb522ea55807378c2c9d9f3f78aa72c2e2bf04
Saikat <sachinsingha7899@gmail.com>
2024-01-13 20:47:35 +0600

    added

--- src/Components/Square/Square.jsx
+++ src/Components/Square/Square.jsx
@@ -3,11 +3,12 @@ import { useState } from "react";
 const Square = () => {
     const [value,setValue] =useState(null);
     function handleClick(){
+        setValue('X');
         
     }
     return (
         <div>
-            <button onClick={handleClick} className="bg-white border border-gray-400 h-10 w-10 m-1 leading-9 text-lg">X</button>
+            <button onClick={handleClick} className="bg-white border border-gray-400 h-10 w-10 m-1 leading-9 text-lg">{value}</button>
         </div>
     );
 };
