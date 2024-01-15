66868cd83996105408c4fcbf41aae32ba7592c59
Saikat <sachinsingha7899@gmail.com>
2024-01-15 16:57:22 +0600

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
-import Square from "./Components/Square/Square";
-
 export default function Board() {
   return (
     <>
-      <div className="flex">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-      <div className="flex">
-        <Square />
-        <Square />
-        <Square />
-      </div>
-      <div className="flex">
-        <Square />
-        <Square />
-        <Square />
+      <div className="text-3xl text-center">
+        Hello People
       </div>
     </>
   );
